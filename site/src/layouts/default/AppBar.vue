3a26c03c157f85829ec2610ddff0a40f43221965
mouper <51354655+mouper@users.noreply.github.com>
2024-04-19 23:50:15 +0800

    add html and css for navbar
    added to appbar.vue, html in template + css in styles, changed off white colour in styles

--- site/src/layouts/default/AppBar.vue
+++ site/src/layouts/default/AppBar.vue
@@ -1,21 +1,72 @@
 // This is the app bar
 
 <template>
-  <v-app-bar flat color="blue">
-    <v-app-bar-title class="title">
-      UWA Marketing Society
-    </v-app-bar-title>
-  </v-app-bar>
+  <body>
+    <img class="logo" src="../../../logo.svg" width="500"/>
+    <nav>
+        <ul>
+            <li>
+                <a href= "#">menu</a>
+            </li>
+        <ul>
+        </ul>
+            <li class="join">
+                <a href= "#">Join Us!</a>
+            </li>
+        </ul>
+    </nav>
+  </body>
 </template>
 
 <style>
-.title {
-  font-family: 'Roxborough';
-  font-weight: bolder;
-}
+  body{ 
+      background-color: #E9E9E9; 
+      font-family: Roxborough;
+  }
 
+  a { 
+      text-decoration: none; 
+      color: #474747;
+      font-size: 20px;
+      font-weight: bold;
+    } 
+    
+  nav ul li:first-child { 
+      float: left; 
+      margin-left: 70px;
+      padding-top: 10px;
+  }
 
+  nav ul li { 
+      list-style:none;
+      margin:40px;
+      float: right; 
+      margin-right: 60px;
+    }
 
+  .join{
+      border-radius: 25px;
+      background: #E59ECD;
+      padding-top: 10px;
+      padding-bottom: 13px;
+      padding-left: 35px;
+      padding-right: 35px;
+      outline-style: solid;
+      outline-color: #474747;
+      outline-width: thin;
+  }
+
+  .logo{
+      position: absolute;
+      left: 30%;
+      right: 30%;
+      width: 40%;
+      padding-top: 30px;
+  }
+
+  .line {
+      border-bottom: 1px solid #888;
+    }
 </style>
 
 <script setup>
