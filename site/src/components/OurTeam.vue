73104b0308e8a8bc29af8bfaf8dc772afc690f76
Connor2803 <87635341+Connor2803@users.noreply.github.com>
2024-06-09 16:52:24 +0800

    Add super basic images

---
+++ site/src/components/OurTeam.vue
@@ -0,0 +1,63 @@
+
+
+<template>
+    
+    <v-container id="OurTeam">
+
+
+            <h1> Our Team </h1>
+
+                    
+            <v-container id="imagecontainer">
+                
+                <img src="@/images/sample_man.jpg" alt="stock man">
+                <img src="@/images/sample_man.jpg" alt="stock man">
+                <img src="@/images/sample_man.jpg" alt="stock man">
+                <img src="@/images/sample_man.jpg" alt="stock man">
+                <img src="@/images/sample_man.jpg" alt="stock man">
+
+            </v-container>
+
+
+
+    </v-container>
+
+
+
+</template>
+
+<style>
+   
+    #OurTeam {
+        background-color: black;
+        border: 1px solid black;     
+    }
+
+
+    #imagecontainer {
+        background-color: red;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+  
+
+    }
+
+    img {
+        width: 14%;
+        height: auto;
+        margin: 1%;
+    }
+
+    h1 {
+        color: orange;
+        background-color: beige;
+        text-align: center;
+    }
+
+
+</style>
+
+<script setup>
+  //
+</script>
