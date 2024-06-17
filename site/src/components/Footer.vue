693dd3674ee45f0e749965bb5ef672ed52fa67c9
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-06-17 21:21:33 +0800

    Add new footer component
    - Import footer component into main file.

---
+++ site/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='footer'>
+    <p>sample</p>
+  </div>
+</template>
+
+<style scoped>
+  .footer {
+    transition: ease all .05s;
+    background-color: #a7a7a7;
+    font-family: Roxborough;
+    height: 2vw;
+    border-top: 1px solid #888;
+    color: #484848;
+    display: flex; 
+    align-items: center;
+    flex-direction: row;
+  }
+</style>
