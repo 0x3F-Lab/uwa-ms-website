21b69d033fdaee98db21e35ff1f816d467e73f32
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-06-19 22:45:48 +0800

    Remove social links and resize footer components
    - Remove instagram and discord links.
    - Remove guild logo.
    - Add spacing on either side of footer for proper margin design.
    - Increase font size.

--- site/src/components/Footer.vue
+++ site/src/components/Footer.vue
@@ -1,87 +1,48 @@
 <template>
-  <div class='footer'>
-    <div>
-      <img id='uwa-guild-logo' src='/uwa-guild-logo.svg' alt="UWA Student Guild Logo">
-    </div>
-    <div>
-      <div id='footer-socials'>
-        <a href="instagram://user?username=uwamarketingsociety" onclick="href='https\://www.instagram.com/uwamarketingsociety/'" target="_blank" rel="noopener noreferrer" id='socials-instagram'>
-          <img src='/instagram-logo.svg' alt="Instagram Logo">
-        </a>
-        <a href="https://discord.com" target="_blank" rel="noopener noreferrer" id='socials-discord'>
-          <img src='/discord-logo.svg' alt="Discord Logo">
-        </a>
-      </div>
+  <div id='footer'>
+    <div id='footer-inner'>
       <div id='footer-copyright'>
-        <p>© 2024 UWA Marketing Society. All Rights Reserved </p>
-        <p>By using this website, you agree to the use of cookies. </p>
+        <p>© 2024 UWA Marketing Society. All Rights Reserved. By using this website, you agree to the use of cookies. </p>
       </div>
-    </div>
-    <div>
-      <img id='uwa-logo' src="/uwa-logo.svg" alt="UWA Logo">
+      <div id='footer-logo'>
+        <img src="/uwa-logo.svg" alt="UWA Logo">
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .footer {
+  #footer {
+    border-left: 3em solid #E9E9E9;
+    border-right: 3em solid #E9E9E9;
+  }
+
+  #footer-inner {
     transition: ease all .05s;
-    background-color: #b6b6b6;
+    background-color: #E9E9E9;
     font-family: Roxborough;
     border-top: 1px solid #888;
     color: #484848;
     display: flex; 
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     text-align: center;
     position: relative;
-    padding: 1em 2.5em;
-    font-size: 0.85em;
+    padding: 1.5em 2em 2em 2em;
+    font-size: 0.9em;
   }
 
-  .footer > div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  #footer-logo {
+    margin-left: auto;
   }
 
-  .footer > div:first-child {
-    position: absolute;
-    left: 3em;
-  }
-
-  .footer > div:last-child {
-    position: absolute;
-    right: 3em;
-  }
-
-  #footer-socials {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: 100%;
-  }
-
-  #footer-socials img {
-    width: 2em;
-  }
-
-  #uwa-guild-logo {
-    width: 6em
-  }
-
-  #uwa-logo {
-    width: 12em;
+  #footer-logo > img {
+    width: 8.5em;
   }
 
   @media only screen and (max-width: 768px) {
-    .footer{ 
-      font-size: 0.65em;
-    }
-
-    .footer > div > svg,
-    .footer > div > img {
-      display: none;
+    #footer{ 
+      font-size: 0.75em;
     }
   }
 </style>
