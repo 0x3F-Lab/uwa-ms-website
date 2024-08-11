5ae1bd23ccdc907e99b7523f068f8133eec908b0
Harry Rigg <riggh@icloud.com>
2024-08-11 10:16:19 +0800

    Test deploy workflow

--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -2,14 +2,15 @@
 
 <template>
   <HeaderSection />
+  Test
   <OurTeam />
   <InstagramPosts />
   <Footer />
 </template>
 
 <script setup>
-  import OurTeam from '@/components/OurTeam.vue'
-  import InstagramPosts from '@/components/InstagramPosts.vue';
-  import HeaderSection from '@/components/HeaderSection.vue'
-  import Footer from '@/components/Footer.vue'
+import OurTeam from "@/components/OurTeam.vue";
+import InstagramPosts from "@/components/InstagramPosts.vue";
+import HeaderSection from "@/components/HeaderSection.vue";
+import Footer from "@/components/Footer.vue";
 </script>
